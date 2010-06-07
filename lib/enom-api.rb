9e630950aac9e82a5043bd9ee79e6ec41deb1516
Geoff Garside <geoff@geoffgarside.co.uk>
2010-06-07 14:07:36 +0100

    Add Client and Registrant to autoloader

--- lib/enom-api.rb
+++ lib/enom-api.rb
@@ -1,4 +1,6 @@
 module EnomAPI
+  autoload :Client, File.dirname(__FILE__) + '/enom-api/client.rb'
   autoload :Interface, File.dirname(__FILE__) + '/enom-api/interface.rb'
+  autoload :Registrant, File.dirname(__FILE__) + '/enom-api/registrant.rb'
   autoload :SearchQuery, File.dirname(__FILE__) + '/enom-api/search_query.rb'
 end
