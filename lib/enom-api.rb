da30cc3ce5f832e8198fac7f799cfc26515ad898
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-25 11:35:57 +0000

    Add documentation for EnomAPI RuntimeError classes

--- lib/enom-api.rb
+++ lib/enom-api.rb
@@ -1,18 +1,20 @@
 require 'xml'
 require 'demolisher'
 
-module EnomAPI
+module EnomAPI  # :nodoc:
   autoload :Client, File.dirname(__FILE__) + '/enom-api/client.rb'
   autoload :Interface, File.dirname(__FILE__) + '/enom-api/interface.rb'
   autoload :Registrant, File.dirname(__FILE__) + '/enom-api/registrant.rb'
   autoload :SearchQuery, File.dirname(__FILE__) + '/enom-api/search_query.rb'
 
+  # API Response error exception
   class ResponseError < RuntimeError
     attr_reader :messages
     def initialize(error_messages)
       @messages = error_messages
     end
   end
+  # API Incomplete response error
   class IncompleteResponseError < RuntimeError
     attr_reader :xml
     def initialize(xml)
