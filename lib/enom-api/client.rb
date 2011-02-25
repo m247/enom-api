be15e155524803018246c354d648a41466aea20e
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-25 14:38:15 +0000

    Fix EnomAPI::Client#get_whois_contact method results parsing

--- lib/enom-api/client.rb
+++ lib/enom-api/client.rb
@@ -477,29 +477,49 @@ module EnomAPI
     # Gets the WHOIS contact details for a domain
     #
     # The returned hash has the following keys
-    # - (Registrant) +:registrant+ -- Registrant object
+    # - (Hash<Symbol,Registrant>) +:contacts+ -- Registrant object
     # - (Time) +:updated_date+ --     Domain last update time
     # - (Time) +:created_date+ --     Domain creation date
     # - (Time) +:expiry_date+ --      Domain expiration date
     # - (Array) +:nameservers+ --     Array of name server names
+    # - (String) +:status+ --         Registrar lock status
     #
     # @param [String] domain Domain name to retrieve WHOIS information for
     # @return [Hash] response data
     def get_whois_contact(domain)
       xml = send_recv(:GetWhoisContact, split_domain(domain))
 
-      out = { :registrant => Registrant.from_xml(xml._current_node.find('//contact[@ContactType="Registrant"]').first) }
-      xml.send("rrp-info") do
-        out[:updated_date] = Time.parse(xml.send("updated-date"))
-        out[:created_date] = Time.parse(xml.send("created-date"))
-        out[:expiry_date]  = Time.parse(xml.send("registration-expiration-date"))
-        out[:nameservers]  = Array.new
-        xml.nameserver do
+      out = {:contacts => {}}
+      xml.GetWhoisContacts do
+        xml.contacts.contact do |contact,_|
+          case contact['ContactType']
+          when 'Registrant'
+            out[:contacts][:registrant] = Registrant.from_xml(contact)
+          when 'Administrative'
+            out[:contacts][:administrative] = Registrant.from_xml(contact)
+          when 'Technical'
+            out[:contacts][:technical] = Registrant.from_xml(contact)
+          when 'Billing'
+            out[:contacts][:billing] = Registrant.from_xml(contact)
+          end
+        end
+
+        xml.send("rrp-info") do
+          out[:updated_date] = Time.parse(xml.send("updated-date"))
+          out[:created_date] = Time.parse(xml.send("created-date"))
+          out[:expiry_date]  = Time.parse(xml.send("registration-expiration-date"))
+          out[:nameservers]  = Array.new
           xml.nameserver do
-            out[:nameservers] << xml.strip
+            xml.nameserver do
+              out[:nameservers] << xml.to_s.downcase
+            end
+          end
+          xml.status do
+            out[:status] = xml.status # != registration status from get_domain_info. = lock status
           end
         end
       end
+
       out
     end
 
