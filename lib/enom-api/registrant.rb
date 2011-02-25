037c6cef9dc4c072c8c94734adfc9cf58d6f8f61
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-25 14:37:14 +0000

    Fix EnomAPI::Registrant.from_xml method

--- lib/enom-api/registrant.rb
+++ lib/enom-api/registrant.rb
@@ -1,22 +1,24 @@
+require 'demolisher'
+
 module EnomAPI
   # Represents a registrant or other type of Contact in the eNom API
   class Registrant
     # @param [Demolisher, String] xmldoc Demolisher or XML String of registrant information
     # @return [Registrant] Registrant composed from the information in the xmldoc
     def self.from_xml(xmldoc)
-      xml = xmldoc.kind_of?(Demolisher) ? xmldoc : Demolisher.demolish(xmldoc)
+      @xml = xml = xmldoc.kind_of?(Demolisher::Node) ? xmldoc : Demolisher.demolish(xmldoc.to_s)
 
-      new(xml.FName, xml.LName) do
-        organisation    = xml.Organization
-        city            = xml.City
-        state           = xml.StateProvince
-        postal_code     = xml.PostalCode
-        phone           = xml.Phone
-        phone_extension = xml.PhoneExtension
-        fax             = xml.Fax
-        email           = xml.EmailAddress
-        address         = [xml.Address1, xml.Address2].join("\n")
-      end
+      r = new(xml.FName, xml.LName)
+      r.organisation    = xml.Organization
+      r.city            = xml.City
+      r.state           = xml.StateProvince
+      r.postal_code     = xml.PostalCode
+      r.phone           = xml.Phone
+      r.phone_extension = xml.PhoneExt
+      r.fax             = xml.Fax
+      r.email           = xml.EmailAddress
+      r.address         = [xml.Address1, xml.Address2].delete_if{|a| a.nil? || a.to_s == ""}.join("\n")
+      r
     end
 
     attr_accessor :firstname, :lastname, :phone, :phone_extension, :fax, :email,
@@ -26,6 +28,8 @@ module EnomAPI
     # @param [String] last Registrant last name
     # @yield block to configure the registrant, any method on Registrant may be called.
     def initialize(first, last, &blk)
+      raise ArgumentError, "first and last may not be nil" unless first && last
+
       @firstname, @lastname = first, last
       instance_eval(&blk) if blk
     end
